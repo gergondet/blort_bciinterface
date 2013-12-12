12d5b0530bac3df8c88bd8c82b5936d536ef1ab9
Pierre Gergondet <pierre.gergondet@aist.go.jp>
2013-12-12 17:19:28 +0900

    Avoid segfault in multi-threaded environment
    Loading camera parameters makes some OpenGL calls under the hood, not very
    multi-thread friendly

--- src/BLORTObject.cpp
+++ src/BLORTObject.cpp
@@ -5,28 +5,6 @@ BLORTObject::BLORTObject(const std::string & object_name, const std::string & fi
   wwidth(wwidth), wheight(wheight), iwidth(iwidth), iheight(iheight),
   object_name(object_name), filename(filename), model(0), last_update(0)
 {
-    TomGine::tgCamera::Parameter camPar;
-    camPar.width = 640;
-    camPar.height = 480;
-    camPar.fx = 538.047058;
-    camPar.fy = 538.226013;
-    camPar.cx = 319.509888;
-    camPar.cy = 239.332718;
-    camPar.k1 = 0.031828;
-    camPar.k2 = -0.106567;
-    camPar.k3 = 0;
-    camPar.p1 = -0.001167;
-    camPar.p2 = 0.002602;
-    camPar.pos.x = 0.31188;
-    camPar.pos.y = -0.3;
-    camPar.pos.z = 0.243049;
-    vec3 rotv; rotv.x = -1.917587; rotv.y = -0.453044; rotv.z = 0.389306;
-    camPar.rot.fromRotVector(rotv);
-    camPar.zNear = 0.1f;
-    camPar.zFar = 5.0f;
-    camera.Load(camPar);
-    pose.t = vec3(0.0, 0.1, 0.0);
-    pose.Rotate(0.0f, 0.0f, 0.5f);
     manager.AddObject(this);
 }
 
@@ -40,6 +18,28 @@ void BLORTObject::Display(sf::RenderWindow * app, unsigned int frameCount, sf::C
 {
     if(!model)
     {
+        TomGine::tgCamera::Parameter camPar;
+        camPar.width = 640;
+        camPar.height = 480;
+        camPar.fx = 538.047058;
+        camPar.fy = 538.226013;
+        camPar.cx = 319.509888;
+        camPar.cy = 239.332718;
+        camPar.k1 = 0.031828;
+        camPar.k2 = -0.106567;
+        camPar.k3 = 0;
+        camPar.p1 = -0.001167;
+        camPar.p2 = 0.002602;
+        camPar.pos.x = 0.31188;
+        camPar.pos.y = -0.3;
+        camPar.pos.z = 0.243049;
+        vec3 rotv; rotv.x = -1.917587; rotv.y = -0.453044; rotv.z = 0.389306;
+        camPar.rot.fromRotVector(rotv);
+        camPar.zNear = 0.1f;
+        camPar.zFar = 5.0f;
+        camera.Load(camPar);
+        pose.t = vec3(0.0, 0.1, 0.0);
+        pose.Rotate(0.0f, 0.0f, 0.5f);
         model = new Tracking::TrackerModel();
         Tracking::ModelLoader loader;
         loader.LoadPly(*model, filename.c_str());
